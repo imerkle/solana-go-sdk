4dce62d53f1f5e675b8ab9d2d35b791a333b08bc
imerkle <imerkle@tutanota.com>
2021-06-14 11:24:11 +0530

    fix rpc results

--- client/get_extras.go
+++ client/get_extras.go
@@ -2,23 +2,49 @@ package client
 
 import (
 	"context"
+	"errors"
 )
 
 func (s *Client) GetGenesisHash(ctx context.Context) (string, error) {
-	var res string
-	_ = s.request(ctx, "getGenesisHash", []interface{}{}, &res)
-
-	return res, nil
+	res := struct {
+		GeneralResponse
+		Result string `json:"result"`
+	}{}
+	err := s.request(ctx, "getGenesisHash", []interface{}{}, &res)
+	if err != nil {
+		return "", err
+	}
+	if res.Error != (ErrorResponse{}) {
+		return "", errors.New(res.Error.Message)
+	}
+	return res.Result, nil
 }
 func (s *Client) GetFirstAvailableBlock(ctx context.Context) (uint64, error) {
-	var res uint64
-	_ = s.request(ctx, "getFirstAvailableBlock", []interface{}{}, &res)
-
-	return res, nil
+	res := struct {
+		GeneralResponse
+		Result uint64 `json:"result"`
+	}{}
+	err := s.request(ctx, "getFirstAvailableBlock", []interface{}{}, &res)
+	if err != nil {
+		return 0, err
+	}
+	if res.Error != (ErrorResponse{}) {
+		return 0, errors.New(res.Error.Message)
+	}
+	return res.Result, nil
 }
-func (s *Client) GetSlot(ctx context.Context) (uint64, error) {
-	var res uint64
-	_ = s.request(ctx, "getSlot", []interface{}{}, &res)
 
-	return res, nil
+func (s *Client) GetSlot(ctx context.Context) (uint64, error) {
+	res := struct {
+		GeneralResponse
+		Result uint64 `json:"result"`
+	}{}
+	err := s.request(ctx, "getSlot", []interface{}{}, &res)
+	if err != nil {
+		return 0, err
+	}
+	if res.Error != (ErrorResponse{}) {
+		return 0, errors.New(res.Error.Message)
+	}
+	return res.Result, nil
 }
